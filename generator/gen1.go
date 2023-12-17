0a06f81fb3e3939590395aa1ec42dde6f416dfbc
DMXMax <29312988+DMXMax@users.noreply.github.com>
2023-12-17 14:10:29 -0800

    Adding FanIn

--- generator/gen1.go
+++ generator/gen1.go
@@ -1,5 +1,7 @@
 package generator
 
+import "sync"
+
 var Repeat = func(done <-chan any, values ...any) <-chan any {
 	valueStream := make(chan any, 2)
 	go func() {
@@ -45,4 +47,32 @@ var RepeatFn = func(done <-chan any, fn func() any) <-chan any {
 		}
 	}()
 	return valueStream
+}
+
+var FanIn = func(done <-chan any, channels ...<-chan any) <-chan any {
+	var wg sync.WaitGroup
+	multiplexedStream := make(chan any)
+
+	multiplex := func(c <-chan any) {
+		defer wg.Done()
+		for i := range c {
+			select {
+			case <-done:
+				return
+			case multiplexedStream <- i:
+			}
+		}
+
+	}
+	wg.Add(len(channels))
+	for _, c := range channels {
+		go multiplex(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(multiplexedStream)
+
+	}()
+	return multiplexedStream
 }
